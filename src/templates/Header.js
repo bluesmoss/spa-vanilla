4017b6b7fa4e307e33bcefedb04ff90344faefff
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-04-08 19:45:32 -0500

    feature(Layout): Add header's template

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+     const view = `
+        <div class="header">
+            <div class="header__logo">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    <a>
+                </h1>
+            </div>
+            <div class="header__nav">
+                <a href="#/about">
+                    About
+                <a>           
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
