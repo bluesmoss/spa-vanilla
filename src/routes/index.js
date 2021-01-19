5b73bf8bbae369a18ff3ed13450af06f04686caa
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-01-18 18:37:35 -0600

    chore(test): add test to identify mobile device

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,7 +24,30 @@ const router = async () => {
     let render = routes[route] ? routes[route] : Error404;
 
     content.innerHTML= await render();
+    mobile();
 
+}
+
+const mobile = () => {
+    let hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+        hasTouchScreen = navigator['msMaxTouchPoints'] > 0;
+    } else {
+        let mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+        if (mQ && mQ.media === "(pointer:coarse)") {
+            hasTouchScreen = !!mQ.matches;
+        } else if ('orientation' in window) {
+            hasTouchScreen = true; // deprecated, but good fallback
+        }
+    }
+    
+    console.log("hasTouchScreen", hasTouchScreen);
+
+    let mob = /Mobi/i.test(window.navigator.userAgent);
+
+    console.log("useragent", mob);
 }
 
 export default router;
